64134e4e32cc155e6428aff1c1b4f1eeca716059
tr <tr@immerda.ch>
2020-08-29 17:17:30 +0200

    Add spec tests it should compile

---
+++ spec/classes/nftables_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'nftables' do
+  let(:pre_condition) { 'Exec{path => "/bin"}' }
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+    end
+  end
+end
