95b1259b853f7459ba81fcb184e8126c00244ce1
tr <tr@immerda.ch>
2020-08-30 12:57:25 +0200

    Add spec tests for ip nat prerouting

--- spec/classes/router_spec.rb
+++ spec/classes/router_spec.rb
@@ -56,6 +56,34 @@ describe 'nftables' do
           :order   => '99',
         )}
 
+        it { is_expected.to contain_concat('nftables-ip-nat-chain-PREROUTING').with(
+          :path           => '/etc/nftables/puppet/ip-nat-chain-PREROUTING.nft',
+          :owner          => 'root',
+          :group          => 'root',
+          :mode           => '0640',
+          :ensure_newline => true,
+        )}
+        it { is_expected.to contain_concat__fragment('nftables-ip-nat-chain-PREROUTING-header').with(
+          :target  => 'nftables-ip-nat-chain-PREROUTING',
+          :content => /^chain PREROUTING {$/,
+          :order   => '00',
+        )}
+        it { is_expected.to contain_concat__fragment('nftables-ip-nat-chain-PREROUTING-rule-type').with(
+          :target  => 'nftables-ip-nat-chain-PREROUTING',
+          :content => /^  type nat hook prerouting priority -100$/,
+          :order   => '01',
+        )}
+        it { is_expected.to contain_concat__fragment('nftables-ip-nat-chain-PREROUTING-rule-policy').with(
+          :target  => 'nftables-ip-nat-chain-PREROUTING',
+          :content => /^  policy accept$/,
+          :order   => '02',
+        )}
+        it { is_expected.to contain_concat__fragment('nftables-ip-nat-chain-PREROUTING-footer').with(
+          :target  => 'nftables-ip-nat-chain-PREROUTING',
+          :content => /^}$/,
+          :order   => '99',
+        )}
+
         it { is_expected.to contain_concat('nftables-ip-nat-chain-POSTROUTING').with(
           :path           => '/etc/nftables/puppet/ip-nat-chain-POSTROUTING.nft',
           :owner          => 'root',
