e73f2e97559f6fe5d44e3919ed56c5de49cee7c5
tr <tr@immerda.ch>
2020-10-28 20:53:26 +0100

    Fix rule node exporter

--- manifests/rules/node_exporter.pp
+++ manifests/rules/node_exporter.pp
@@ -3,10 +3,10 @@ class nftables::rules::node_exporter(
   Variant[String,Array[String,1]]
     $prometheus,
   Integer
-    $puppetserver_port = 9100,
+    $port = 9100,
 ) {
   nftables::rule{
     'default_in-node_exporter':
-      content => "tcp dport {${join($ports,', ')}} accept",
+      content => "ip saddr ${prometheus} tcp dport ${port} accept",
   }
 }
