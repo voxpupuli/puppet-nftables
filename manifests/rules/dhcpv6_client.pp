43566263084ef5c1686086215e759728dcf63439
Nacho Barrientos <nacho.barrientos@cern.ch>
2020-11-13 18:24:19 +0100

    Add rules for outgoing and incoming DHCPv6 client traffic

---
+++ manifests/rules/dhcpv6_client.pp
@@ -0,0 +1,6 @@
+class nftables::rules::dhcpv6_client {
+  nftables::rule{
+    'default_in-dhcpv6_client':
+      content => 'ip6 saddr fe80::/10 ip6 daddr fe80::/10 udp sport 547 udp dport 546 accept',
+  }
+}
