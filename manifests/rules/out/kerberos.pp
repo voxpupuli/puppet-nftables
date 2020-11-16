215aee133455edad234c3f920f3b21ed051ee223
Steve Traylen <steve.traylen@cern.ch>
2020-11-16 13:02:11 +0100

    Add kerberos out and openafs_client out

---
+++ manifests/rules/out/kerberos.pp
@@ -0,0 +1,11 @@
+# @summary allows outbound access for kerberos
+class nftables::rules::out::kerberos {
+
+  nftables::rule{
+    'default_out-kerberos_udp':
+       content =>  'udp dport 88 accept';
+    'default_out-kerberos_tcp':
+       content =>  'tcp dport 88 accept';
+  }
+
+}
