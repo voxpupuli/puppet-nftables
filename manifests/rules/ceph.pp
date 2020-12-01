bbc93ede0da9e028359665bead74d6cd8880a5aa
Dan van der Ster <daniel.vanderster@cern.ch>
2020-12-01 10:33:12 +0100

    Add ceph related rules
    Signed-off-by: Dan van der Ster <daniel.vanderster@cern.ch>

---
+++ manifests/rules/ceph.pp
@@ -0,0 +1,9 @@
+# Ceph is a distributed object store and file system.
+# Enable this to support Ceph's Object Storage Daemons (OSD),
+# Metadata Server Daemons (MDS), or Manager Daemons (MGR).
+class nftables::rules::ceph {
+  nftables::rule{
+    'default_in-ceph':
+      content => 'tcp dport 6800-7300 accept comment "Accept Ceph OSD, MDS, MGR"',
+  }
+}
