2ae2a0d0d0826870284701de3e2ced7584ff314f
Nacho Barrientos <nacho.barrientos@cern.ch>
2020-12-02 14:30:44 +0100

    Squashed 'code/' changes from c3be15e..ed8e464
    ed8e464 Merge pull request #32 from dvanders/ceph_nfs
    d0c972c Test NFS rules
    f4e9e99 Test ceph rules
    5210e02 Add NFS-related rules
    bbc93ed Add ceph related rules
    
    git-subtree-dir: code
    git-subtree-split: ed8e46435587f093ee845ac1a78c01b10cd8cb06

---
+++ manifests/rules/ceph_mon.pp
@@ -0,0 +1,10 @@
+# Ceph is a distributed object store and file system.
+# Enable this option to support Ceph's Monitor Daemon.
+class nftables::rules::ceph_mon(
+  Array[Integer,1] $ports = [3300, 6789],
+){
+  nftables::rule{
+    'default_in-ceph_mon':
+      content => "tcp dport {${$ports.join(', ')}} accept comment \"Accept Ceph MON\"",
+  }
+}
---
+++ manifests/rules/out/ceph_client.pp
@@ -0,0 +1,12 @@
+# Ceph is a distributed object store and file system.
+# Enable this to be a client of Ceph's Monitor (MON),
+# Object Storage Daemons (OSD), Metadata Server Daemons (MDS),
+# and Manager Daemons (MGR).
+class nftables::rules::out::ceph_client(
+  Array[Integer,1] $ports = [3300, 6789],
+){
+  nftables::rule{
+    'default_out-ceph_client':
+      content => "tcp dport { ${$ports.join(', ')}, 6800-7300 } accept comment \"Accept Ceph MON, OSD, MDS, MGR\"",
+  }
+}
