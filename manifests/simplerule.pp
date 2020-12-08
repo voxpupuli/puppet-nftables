467ea4e227f00b4d510b3153bb71d871edb5a63f
Nacho Barrientos <nacho.barrientos@cern.ch>
2020-12-08 12:12:37 +0100

    Lint fixes

--- manifests/simplerule.pp
+++ manifests/simplerule.pp
@@ -10,39 +10,26 @@
 #     daddr   => '2001:1458::/32',
 #   }
 
-define nftables::simplerule(
-  Enum['present','absent']
-    $ensure = 'present',
-  Pattern[/^[-a-zA-Z0-9_]+$/]
-    $rulename = $title,
-  Pattern[/^\d\d$/]
-    $order = '50',
-  String
-    $chain  = 'default_in',
-  String
-    $table = 'inet-filter',
-  Enum['accept', 'drop']
-    $action = 'accept',
-  Optional[String]
-    $comment = undef,
-  Optional[Variant[Array[Stdlib::Port, 1], Stdlib::Port, Pattern[/\d+-\d+/]]]
-    $dport  = undef,
-  Optional[Enum['tcp', 'tcp4', 'tcp6', 'udp', 'udp4', 'udp6']]
-    $proto  = undef,
-  Optional[Variant[Stdlib::IP::Address::V6, Stdlib::IP::Address::V4, Pattern[/^@[-a-zA-Z0-9_]+$/]]]
-    $daddr = undef,
-  Enum['ip', 'ip6']
-    $set_type = 'ip6',
-  Boolean
-    $counter = false,
-){
-
+define nftables::simplerule (
+  Enum['present','absent'] $ensure = 'present',
+  Pattern[/^[-a-zA-Z0-9_]+$/] $rulename = $title,
+  Pattern[/^\d\d$/] $order = '50',
+  String $chain  = 'default_in',
+  String $table = 'inet-filter',
+  Enum['accept', 'drop'] $action = 'accept',
+  Optional[String] $comment = undef,
+  Optional[Variant[Array[Stdlib::Port, 1], Stdlib::Port, Pattern[/\d+-\d+/]]] $dport  = undef,
+  Optional[Enum['tcp', 'tcp4', 'tcp6', 'udp', 'udp4', 'udp6']] $proto  = undef,
+  Optional[Variant[Stdlib::IP::Address::V6, Stdlib::IP::Address::V4, Pattern[/^@[-a-zA-Z0-9_]+$/]]] $daddr = undef,
+  Enum['ip', 'ip6'] $set_type = 'ip6',
+  Boolean $counter = false,
+) {
   if $dport and !$proto {
     fail('Specifying a transport protocol via $proto is mandatory when passing a $dport')
   }
 
   if $ensure == 'present' {
-    nftables::rule{"${chain}-${rulename}":
+    nftables::rule { "${chain}-${rulename}":
       content => epp('nftables/simplerule.epp',
         {
           'action'   => $action,
