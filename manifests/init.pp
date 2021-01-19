de7a904c04397b5a4b808fb26765d9026e344890
Kienan Stewart <kienan@koumbit.org>
2021-01-19 17:34:18 -0500

    Move configuration_path default to module data

--- manifests/init.pp
+++ manifests/init.pp
@@ -81,7 +81,8 @@
 #   Specify hashes of `nftables::rule`s via hiera
 #
 # @param configuration_path
-#   The absolute path to the principal nftables configuration file.
+#   The absolute path to the principal nftables configuration file. The default
+#   varies depending on the system, and is set in the module's data.
 #
 class nftables (
   Boolean $in_ssh = true,
@@ -102,7 +103,7 @@ class nftables (
   Variant[Boolean[false], Pattern[/icmp(v6|x)? type .+|tcp reset/]] $reject_with = 'icmpx type port-unreachable',
   Variant[Boolean[false], Enum['mask']] $firewalld_enable = 'mask',
   Optional[Array[Pattern[/^(ip|ip6|inet)-[-a-zA-Z0-9_]+$/],1]] $noflush_tables = undef,
-  Stdlib::Unixpath $configuration_path = '/etc/sysconfig/nftables.conf',
+  Stdlib::Unixpath $configuration_path,
 ) {
   package { 'nftables':
     ensure => installed,
