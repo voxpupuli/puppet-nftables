f29ea0294bd44d9e5816a8c7f665c4ee6f2518fa
Kienan Stewart <kienan@koumbit.org>
2021-01-25 17:38:30 -0500

    Fix nftables::rules::out::nfs3 for nftables 0.9.0

--- manifests/rules/out/nfs3.pp
+++ manifests/rules/out/nfs3.pp
@@ -2,6 +2,6 @@
 class nftables::rules::out::nfs3 {
   nftables::rule {
     'default_out-nfs3':
-      content => 'meta l4proto { tcp, udp } th dport nfs accept comment "Accept NFS3"',
+      content => 'meta l4proto { tcp, udp } @th,16,16 2049 accept comment "Accept NFS3"',
   }
 }
