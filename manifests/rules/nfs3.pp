df9a09bb3b8bde7c47097fee9779a12ee2f1bfd3
Kienan Stewart <kienan@koumbit.org>
2021-01-25 16:33:50 -0500

    Fix nftables::rules::nfs3 syntax to work with nftables 0.9.0

--- manifests/rules/nfs3.pp
+++ manifests/rules/nfs3.pp
@@ -2,6 +2,7 @@
 class nftables::rules::nfs3 {
   nftables::rule {
     'default_in-nfs3':
-      content => 'meta l4proto { tcp, udp } th dport nfs accept comment "Accept NFS3"',
+      # To support nftables >= 0.9.0, but < 0.9.3, `@th,16,16 2049` is used instead of `th dport nfs`
+      content => 'meta l4proto { tcp, udp } @th,16,16 2049 accept comment "Accept NFS3"',
   }
 }
