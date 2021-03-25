d48af6c824c94630e7d328249c98de75e129ad9e
Nacho Barrientos <nacho.barrientos@cern.ch>
2021-03-25 13:59:47 +0100

    Squashed 'code/' changes from 05c7f19..7b8e463
    cae7912 Merge pull request #86 from cernops/release_1_3_0
    804b96e Prepare release 1.3.0
    3f2f50c Merge pull request #85 from cernops/qemu
    cd2a3cb Add rules for QEMU/libvirt guests
    18b211e Merge pull request #80 from luisfdez/dockerce
    1bf717d Add optional handling of chains
    c86e270 Merge pull request #84 from traylenator/version
    4d95ea8 Add fact section to README.md
    9dca9bc Fix doc defaults
    032387d Add nftables.version to structured fact.
    b61ccb4 Fix rulename spec in spec
    283e1c3 Fix syntax
    c351549 Add newline & more tests
    6be2adf Add Docker-CE default rules
    7a77d75 Merge pull request #82 from cernops/ibarrien_activemq
    771b325 Add rules for Apache ActiveMQ
    502b9da Merge pull request #81 from cernops/emacs_readme
    b1b6150 Add pointer to Yasnippets for some defined types
    2fda87a Improve sections' formatting
    812ca77 Release 1.2.1-rc0 (#77)
    
    git-subtree-dir: code
    git-subtree-split: 7b8e4635092853a892bd247a05e0025492262b6a

---
+++ spec/classes/rules/activemq_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe 'nftables::rules::activemq' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      context 'default options' do
+        it { is_expected.to compile }
+        it { is_expected.to contain_nftables__rule('default_in-activemq_tcp').with_content('tcp dport 61616 accept') }
+        it { is_expected.to contain_nftables__rule('default_in-activemq_udp').with_content('udp dport 61616 accept') }
+      end
+
+      context 'with tcp set to false' do
+        let(:params) do
+          {
+            tcp: false,
+          }
+        end
+
+        it { is_expected.to compile }
+        it { is_expected.not_to contain_nftables__rule('default_in-activemq_tcp').with_content('tcp dport 61616 accept') }
+        it { is_expected.to contain_nftables__rule('default_in-activemq_udp').with_content('udp dport 61616 accept') }
+      end
+
+      context 'with udp set to false' do
+        let(:params) do
+          {
+            udp: false,
+          }
+        end
+
+        it { is_expected.to compile }
+        it { is_expected.to contain_nftables__rule('default_in-activemq_tcp').with_content('tcp dport 61616 accept') }
+        it { is_expected.not_to contain_nftables__rule('default_in-activemq_udp').with_content('udp dport 61616 accept') }
+      end
+    end
+  end
+end
---
+++ spec/classes/rules/docker_ce_spec.rb
@@ -0,0 +1,156 @@
+require 'spec_helper'
+
+describe 'nftables::rules::docker_ce' do
+  let(:pre_condition) { 'include nftables' }
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      context 'default options' do
+        it { is_expected.to compile }
+        it { is_expected.to contain_nftables__chain('DOCKER') }
+        it { is_expected.to contain_nftables__chain('DOCKER_ISOLATION_STAGE_1') }
+        it { is_expected.to contain_nftables__chain('DOCKER_ISOLATION_STAGE_2') }
+        it { is_expected.to contain_nftables__chain('DOCKER_USER') }
+        it {
+          is_expected.to contain_nftables__chain('DOCKER-nat').with(
+            chain: 'DOCKER',
+            table: 'ip-nat',
+          )
+        }
+        it {
+          is_expected.to contain_nftables__chain('OUTPUT-nat').with(
+            chain: 'OUTPUT',
+            table: 'ip-nat',
+          )
+        }
+        it {
+          is_expected.to contain_nftables__chain('INPUT-nat').with(
+            chain: 'INPUT',
+            table: 'ip-nat',
+          )
+        }
+        it { is_expected.to contain_nftables__rule('DOCKER_ISOLATION_STAGE_1-iifname').with_content('iifname "docker0" oifname != "docker0" counter jump DOCKER_ISOLATION_STAGE_2') }
+        it { is_expected.to contain_nftables__rule('DOCKER_ISOLATION_STAGE_1-counter').with_content('counter return') }
+        it { is_expected.to contain_nftables__rule('DOCKER_ISOLATION_STAGE_2-drop').with_content('oifname "docker0" counter drop') }
+        it { is_expected.to contain_nftables__rule('DOCKER_ISOLATION_STAGE_2-counter').with_content('counter return') }
+        it { is_expected.to contain_nftables__rule('DOCKER_USER-counter').with_content('counter return') }
+        it { is_expected.to contain_nftables__rule('default_fwd-jump_docker_user').with_content('counter jump DOCKER_USER') }
+        it { is_expected.to contain_nftables__rule('default_fwd-jump_docker_isolation_stage_1').with_content('counter jump DOCKER_ISOLATION_STAGE_1') }
+        it { is_expected.to contain_nftables__rule('default_fwd-out_docker_accept').with_content('oifname "docker0" ct state established,related counter accept') }
+        it { is_expected.to contain_nftables__rule('default_fwd-jump_docker').with_content('oifname "docker0" counter jump DOCKER') }
+        it { is_expected.to contain_nftables__rule('default_fwd-idocker_onot_accept').with_content('iifname "docker0" oifname != "docker0" counter accept') }
+        it { is_expected.to contain_nftables__rule('default_fwd-idocker_odocker_accept').with_content('iifname "docker0" oifname "docker0" counter accept') }
+
+        it {
+          is_expected.to contain_nftables__rule('POSTROUTING-docker').with(
+            content: 'oifname != "docker0" ip saddr 172.17.0.0/16 counter masquerade',
+            table: 'ip-nat',
+          )
+        }
+        it {
+          is_expected.to contain_nftables__rule('PREROUTING-docker').with(
+            content: 'fib daddr type local counter jump DOCKER',
+            table: 'ip-nat',
+          )
+        }
+        it {
+          is_expected.to contain_nftables__rule('OUTPUT-jump_docker@ip-nat').with(
+            rulename: 'OUTPUT-jump_docker',
+            content: 'ip daddr != 127.0.0.0/8 fib daddr type local counter jump DOCKER',
+            table: 'ip-nat',
+          )
+        }
+        it {
+          is_expected.to contain_nftables__rule('DOCKER-counter').with(
+            content: 'iifname "docker0" counter return',
+            table: 'ip-nat',
+          )
+        }
+        it {
+          is_expected.to contain_nftables__rule('INPUT-type@ip-nat').with(
+            rulename: 'INPUT-type',
+            content: 'type nat hook input priority 100',
+            table: 'ip-nat',
+          )
+        }
+        it {
+          is_expected.to contain_nftables__rule('INPUT-policy@ip-nat').with(
+            rulename: 'INPUT-policy',
+            content: 'policy accept',
+            table: 'ip-nat',
+          )
+        }
+      end
+
+      context 'with base chain management false' do
+        let(:params) do
+          {
+            manage_base_chains: false,
+          }
+        end
+
+        it { is_expected.to compile }
+
+        it { is_expected.to contain_nftables__chain('DOCKER') }
+        it { is_expected.to contain_nftables__chain('DOCKER_ISOLATION_STAGE_1') }
+        it { is_expected.to contain_nftables__chain('DOCKER_ISOLATION_STAGE_2') }
+        it { is_expected.to contain_nftables__chain('DOCKER_USER') }
+        it { is_expected.to contain_nftables__chain('DOCKER-nat') }
+
+        it { is_expected.not_to contain_nftables__chain('OUTPUT-nat') }
+        it { is_expected.not_to contain_nftables__chain('INPUT-nat') }
+      end
+
+      context 'with docker chain management false' do
+        let(:params) do
+          {
+            manage_docker_chains: false,
+          }
+        end
+
+        it { is_expected.to compile }
+
+        it { is_expected.not_to contain_nftables__chain('DOCKER') }
+        it { is_expected.not_to contain_nftables__chain('DOCKER_ISOLATION_STAGE_1') }
+        it { is_expected.not_to contain_nftables__chain('DOCKER_ISOLATION_STAGE_2') }
+        it { is_expected.not_to contain_nftables__chain('DOCKER_USER') }
+        it { is_expected.not_to contain_nftables__chain('DOCKER-nat') }
+
+        it { is_expected.to contain_nftables__chain('OUTPUT-nat') }
+        it { is_expected.to contain_nftables__chain('INPUT-nat') }
+      end
+
+      context 'with custom interface and subnet' do
+        let(:params) do
+          {
+            docker_interface: 'ifdo0',
+            docker_prefix: '192.168.4.0/24',
+          }
+        end
+
+        it { is_expected.to compile }
+        it { is_expected.to contain_nftables__rule('DOCKER_ISOLATION_STAGE_1-iifname').with_content('iifname "ifdo0" oifname != "ifdo0" counter jump DOCKER_ISOLATION_STAGE_2') }
+        it { is_expected.to contain_nftables__rule('DOCKER_ISOLATION_STAGE_2-drop').with_content('oifname "ifdo0" counter drop') }
+        it { is_expected.to contain_nftables__rule('default_fwd-out_docker_accept').with_content('oifname "ifdo0" ct state established,related counter accept') }
+        it { is_expected.to contain_nftables__rule('default_fwd-jump_docker').with_content('oifname "ifdo0" counter jump DOCKER') }
+        it { is_expected.to contain_nftables__rule('default_fwd-idocker_onot_accept').with_content('iifname "ifdo0" oifname != "ifdo0" counter accept') }
+        it { is_expected.to contain_nftables__rule('default_fwd-idocker_odocker_accept').with_content('iifname "ifdo0" oifname "ifdo0" counter accept') }
+
+        it {
+          is_expected.to contain_nftables__rule('POSTROUTING-docker').with(
+            content: 'oifname != "ifdo0" ip saddr 192.168.4.0/24 counter masquerade',
+            table: 'ip-nat',
+          )
+        }
+        it {
+          is_expected.to contain_nftables__rule('DOCKER-counter').with(
+            content: 'iifname "ifdo0" counter return',
+            table: 'ip-nat',
+          )
+        }
+      end
+    end
+  end
+end
