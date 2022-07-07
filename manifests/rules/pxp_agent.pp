9ad647841f323177bc84d0e8b92b3837514e70df
Tim Meusel <tim@bastelfreak.de>
2022-07-07 17:04:35 +0200

    Update manifests/rules/pxp_agent.pp
    Co-authored-by: Steve Traylen <steve.traylen@cern.ch>

--- manifests/rules/pxp_agent.pp
+++ manifests/rules/pxp_agent.pp
@@ -1,7 +1,7 @@
 # @summary manage in pxp-agent
 # @param ports pxp server ports
 class nftables::rules::pxp_agent (
-  Array[Stdlib::Port] $ports = [8142],
+  Array[Stdlib::Port,1] $ports = [8142],
 ) {
   nftables::rule {
     'default_in-pxp_agent':
