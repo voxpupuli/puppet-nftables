7937a13b87f4d81dbbd099fd2c6921b5a6a65712
Tim Meusel <tim@bastelfreak.de>
2022-07-08 13:12:02 +0200

    chrony: Allow filtering for outgoing NTP servers

--- manifests/rules/out/chrony.pp
+++ manifests/rules/out/chrony.pp
@@ -1,7 +1,25 @@
-# manage out chrony
-class nftables::rules::out::chrony {
-  nftables::rule {
-    'default_out-chrony':
-      content => 'udp dport 123 accept',
+# @summary manage out chrony
+# @param servers single IP-Address or array of IP-addresses from NTP servers
+class nftables::rules::out::chrony (
+  Array[Stdlib::IP::Address] $servers = [],
+) {
+  if empty($servers) {
+    nftables::rule {
+      'default_out-chrony':
+        content => 'udp dport 123 accept',
+    }
+  } else {
+    $ipv6_servers = $servers.filter |$ip| { $ip =~ Stdlib::IP::Address::V6 }
+    $ipv4_servers = $servers.filter |$ip| { $ip =~ Stdlib::IP::Address::V4 }
+    unless empty($ipv6_servers) {
+      nftables::rule { 'default_out-chrony_v6':
+        content => "ip6 daddr {${join($ipv6_servers, ',')}} udp dport 123 accept",
+      }
+    }
+    unless empty($ipv4_servers) {
+      nftables::rule { 'default_out-chrony_v4':
+        content => "ip daddr {${join($ipv4_servers, ',')}} udp dport 123 accept",
+      }
+    }
   }
 }
