c2e342b2dda892861a15f54760c93bbe82c90d39
Tim Meusel <tim@bastelfreak.de>
2023-08-10 01:20:14 +0200

    mDNS: Allow udp port 5353

--- manifests/rules/mdns.pp
+++ manifests/rules/mdns.pp
@@ -2,7 +2,10 @@
 # @summary allow incoming multicast DNS
 #
 class nftables::rules::mdns {
-  nftables::rule { 'default_in-mdns':
+  nftables::rule { 'default_in-mdns1':
     content => 'ip daddr 224.0.0.251 accept',
+  }
+  nftables::rule { 'default_in-mdns2':
+    content => 'udp sport 5353 udp dport 5353 accept',
   }
 }
