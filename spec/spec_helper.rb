65ed81ba2f490e8016946b7f4923bf9f276d808e
Tim Meusel <tim@bastelfreak.de>
2024-07-09 16:14:57 +0200

    partial modulesync 9.1.0
    This excludes the Gemfile changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,24 +3,17 @@
 # Managed by modulesync - DO NOT EDIT
 # https://voxpupuli.org/docs/updating-files-managed-with-modulesync/
 
-RSpec.configure do |c|
-  c.before do
-    # select the systemd service provider even when on docker
-    # https://tickets.puppetlabs.com/browse/PUP-11167
-    if defined?(facts) && %w[Archlinux RedHat].include?(facts[:os]['family'])
-      allow(Puppet::FileSystem).to receive(:exist?).and_call_original
-      allow(Puppet::FileSystem).to receive(:exist?).with('/proc/1/comm').and_return(true)
-      allow(Puppet::FileSystem).to receive(:read).and_call_original
-      allow(Puppet::FileSystem).to receive(:read).with('/proc/1/comm').and_return(['systemd'])
-    end
-  end
-end
-
 # puppetlabs_spec_helper will set up coverage if the env variable is set.
 # We want to do this if lib exists and it hasn't been explicitly set.
 ENV['COVERAGE'] ||= 'yes' if Dir.exist?(File.expand_path('../lib', __dir__))
 
 require 'voxpupuli/test/spec_helper'
+
+RSpec.configure do |c|
+  c.facterdb_string_keys = false
+end
+
+add_mocked_facts!
 
 if File.exist?(File.join(__dir__, 'default_module_facts.yml'))
   facts = YAML.safe_load(File.read(File.join(__dir__, 'default_module_facts.yml')))
@@ -28,3 +21,4 @@ if File.exist?(File.join(__dir__, 'default_module_facts.yml'))
     add_custom_fact name.to_sym, value
   end
 end
+Dir['./spec/support/spec/**/*.rb'].sort.each { |f| require f }
