9d02e9f82b0c622c06e6d0c441a449f6bba3cdcd
Stéphanie Jaumotte <stephanie.jaumotte@insee.fr>
2024-07-31 10:45:51 +0200

    Add variant array

--- spec/type_aliases/nftables_addr_spec.rb
+++ spec/type_aliases/nftables_addr_spec.rb
@@ -8,7 +8,12 @@ describe 'Nftables::Addr' do
   it { is_expected.to allow_value('2001:1458::/32') }
   it { is_expected.to allow_value('2001:1458::3') }
   it { is_expected.to allow_value('@set_name') }
+  it { is_expected.to allow_value(['127.0.0.1']) }
+  it { is_expected.to allow_value(['172.16.1.0/24']) }
+  it { is_expected.to allow_value(['2001:1458::/32']) }
+  it { is_expected.to allow_value(['2001:1458::3']) }
+  it { is_expected.to allow_value(['@set_name']) }
+  it { is_expected.to allow_value(['@set_name', '@set_name2']) }
   it { is_expected.not_to allow_value('anything') }
   it { is_expected.not_to allow_value(43) }
-  it { is_expected.not_to allow_value(['127.0.0.1']) }
 end
